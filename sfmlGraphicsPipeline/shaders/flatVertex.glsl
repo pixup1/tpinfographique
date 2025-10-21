3cf60ba24aab3e8461e9bb13451271cce3421f9b
Ecnama <amance.github@famille-graindorge.fr>
2025-10-21 10:18:41 +0200

    TP2-Ex3-fix normals

--- sfmlGraphicsPipeline/shaders/flatVertex.glsl
+++ sfmlGraphicsPipeline/shaders/flatVertex.glsl
@@ -9,7 +9,8 @@ out vec4 surfel_color;
 void main()
 {
     gl_Position = projMat*viewMat*modelMat*vec4(vPosition, 1.0f);
+    vec3 norm = normalize(transpose(inverse(mat3(modelMat))) * vNormal);
     int w = 1;
-    vec3 cNormal = vec3(vNormal[0] * 0.5 + 0.5, vNormal[1] * 0.5 + 0.5, vNormal[2] * 0.5 + 0.5);
+    vec3 cNormal = vec3(norm[0] * 0.5 + 0.5, norm[1] * 0.5 + 0.5, norm[2] * 0.5 + 0.5);
     surfel_color = vec4(cNormal, w);
 }
