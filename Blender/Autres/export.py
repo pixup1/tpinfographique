2648233166fbd35bb9057cff028726934b4bf9dd
pixup1 <pixup1@yahoo.com>
2025-11-23 23:58:23 +0100

    idk i did a bunch of stuff

--- Blender/Autres/export.py
+++ Blender/Autres/export.py
@@ -1,10 +1,14 @@
 # pyright: reportOptionalMemberAccess=false
 
 import bpy
+from bpy_extras import anim_utils
 import mathutils
+import math
 
 abs_path = bpy.path.abspath("//")
-export_path = f"{abs_path}/../sampleProject/ObjFiles"
+project_path = f"{abs_path}/../sampleProject/"
+obj_path = f"{project_path}/ObjFiles/"
+anim_path = f"{project_path}/Animation/"
 
 def frame_to_time(frame: int):
     return (frame - bpy.context.scene.frame_start) / bpy.context.scene.render.fps
@@ -13,13 +17,12 @@ def export_mesh(obj):
     for ob in bpy.data.objects:
         ob.select_set(False)
     obj.select_set(True)
-    backup_matrix = obj.matrix_world.copy()
-    obj.matrix_world = mathutils.Matrix.Identity(4)
     bpy.ops.wm.obj_export(
-        filepath=f"{export_path}/{obj.name}.obj",
+        filepath=f"{obj_path}/{obj.name}.obj",
         apply_modifiers=True,
         export_selected_objects=True,
-        export_materials=False
+        export_materials=False,
+        apply_transform=False
     )
     y_up_rot = mathutils.Matrix((
         (1, 0, 0, 0),
@@ -27,41 +30,59 @@ def export_mesh(obj):
         (0, -1, 0, 0),
         (0, 0, 0, 1)
     ))
-    y_up_matrix = y_up_rot @ backup_matrix @ y_up_rot.inverted()
-    with open(f"{export_path}/{obj.name}.obj", "a") as f:
+    y_up_matrix = y_up_rot @ obj.matrix_world @ y_up_rot.inverted()
+    with open(f"{obj_path}/{obj.name}.obj", "a") as f:
         f.write("\nTRANSFORM")
         for row in y_up_matrix:
             for val in row:
                 f.write(f" {val}")
         f.write("\n")
-    obj.matrix_world = backup_matrix
     obj.select_set(False)
 
 def export_animation(obj):
     output = ""
-    if obj.animation_data and obj.animation_data.action:
+    anim_data = obj.animation_data
+    if anim_data is not None and anim_data.action is not None:
+        action = anim_data.action
+        slot = anim_data.action_slot
+        if slot is None:
+            slot = anim_data.action_suitable_slots[0]
+            anim_data.action_slot = slot
+        channelbag = anim_utils.action_get_channelbag_for_slot(action, slot)
         frames = {}
-        for fcurve in obj.animation_data.action.fcurves:
+        for fcurve in channelbag.fcurves: # pyright: ignore[reportAttributeAccessIssue]
             for keyframe in fcurve.keyframe_points:
-                #frames.append((int(keyframe.co.x)))
-                if int(keyframe.co.x) not in frames:
-                    frames[int(keyframe.co.x)] = [keyframe.interpolation]
+                frame = int(keyframe.co.x)
+                interp = keyframe.interpolation
+                if frame not in frames:
+                    frames[frame] = [interp]
                 else:
-                    frames[int(keyframe.co.x)].append(keyframe.interpolation)
+                    frames[frame].append(interp)
         scene = bpy.context.scene
         depsgraph = bpy.context.evaluated_depsgraph_get()
+        prev_rot = None
         for fr in sorted(frames.keys()):
             scene.frame_set(fr)
             eval_obj = obj.evaluated_get(depsgraph)
             loc, rot, scale = eval_obj.matrix_world.decompose()
+            if obj.type == 'CAMERA': # Cameras aren't the same in sfml and Blender
+                rot = rot @ mathutils.Euler((math.radians(-90), 0, 0), 'XYZ').to_quaternion()
+            # Ensure quaternion continuity to avoid flipping
+            if prev_rot is not None and rot.dot(prev_rot) < 0: # ngl copilot did this so idk (it avoids the camera flipping)
+                rot = -rot
+            prev_rot = rot.copy()
             t = frame_to_time(fr)
             most_frequent_interp = max(set(frames[fr]), key=frames[fr].count)
             output += f"{t},{most_frequent_interp},{loc.x},{loc.y},{loc.z},{rot.x},{rot.y},{rot.z},{rot.w},{scale.x},{scale.y},{scale.z}\n"
         if output != "":
-            with open(f"{export_path}/{obj.name}.animation", "w") as f:
+            with open(f"{anim_path}/{obj.name}.animation", "w") as f:
                 f.write(output)
 
-if bpy.context.scene is not None:
-    for obj in bpy.data.objects:
-        export_mesh(obj)
+col = bpy.data.collections["EXPORT"]
+if col is not None:
+    for obj in col.objects:
+        if obj.type != 'CAMERA':
+            export_mesh(obj)
         export_animation(obj)
+else:
+    print("No 'EXPORT' collection found.")
--- sampleProject/tortuekaizen.cpp
+++ sampleProject/tortuekaizen.cpp
@@ -1,41 +1,100 @@
 #include <CylinderMeshRenderable.hpp>
 #include <FrameRenderable.hpp>
 #include <MeshRenderable.hpp>
+#include <lighting/LightedMeshRenderable.hpp>
 #include <ShaderProgram.hpp>
 #include <Viewer.hpp>
+#include <lighting/PointLightRenderable.hpp>
+#include <lighting/DirectionalLightRenderable.hpp>
+#include <texturing/CubeMapRenderable.hpp>
+#include <fstream>
+#include <iostream>
 
-void initialize_scene(Viewer& viewer)
+LightedMeshRenderablePtr add_object(Viewer &viewer,
+									const std::string &name,
+									const MaterialPtr &material,
+									ShaderProgramPtr &shaderProgram,
+									HierarchicalRenderablePtr parent = nullptr)
 {
-	// Create a shader program
-	ShaderProgramPtr flatShader = std::make_shared<ShaderProgram>(
-	    "../../sfmlGraphicsPipeline/shaders/flatVertex.glsl",
-	    "../../sfmlGraphicsPipeline/shaders/flatFragment.glsl");
+	std::string obj_path = "../ObjFiles/" + name + ".obj";
+	std::ifstream file(obj_path);
+	if (!file.good())
+	{
+		std::cerr << "Error: File " << obj_path << " does not exist." << std::endl;
+		return nullptr;
+	}
+	LightedMeshRenderablePtr obj = std::make_shared<LightedMeshRenderable>(shaderProgram, obj_path, material);
+	if (parent != nullptr)
+	{
+		HierarchicalRenderable::addChild(parent, obj, true);
+	}
+	std::string anim_path = "../Animation/" + name + ".animation";
+	std::ifstream anim_file(anim_path);
+	if (anim_file.good())
+	{
+		obj->addKeyframesFromFile(anim_path, 0.0, false);
+	}
+	viewer.addRenderable(obj);
 
-	// Add the shader program to the viewer
-	viewer.addShaderProgram(flatShader);
+	return obj;
+}
 
-	const std::string shell_path = "../ObjFiles/Carapace";
-	const std::string nag_ard_path = "../ObjFiles/Nag-ArD";
-	const std::string nag_arg_path = "../ObjFiles/Nag-ArG";
-	const std::string nag_avd_path = "../ObjFiles/Nag-AvD";
-	const std::string nag_avg_path = "../ObjFiles/Nag-AvG";
-	const std::string tete_path = "../ObjFiles/Tete";
-	MeshRenderablePtr shell = std::make_shared<MeshRenderable>(flatShader, shell_path + ".obj");
-	MeshRenderablePtr nag_ard = std::make_shared<MeshRenderable>(flatShader, nag_ard_path + ".obj");
-	MeshRenderablePtr nag_arg = std::make_shared<MeshRenderable>(flatShader, nag_arg_path + ".obj");
-	MeshRenderablePtr nag_avd = std::make_shared<MeshRenderable>(flatShader, nag_avd_path + ".obj");
-	MeshRenderablePtr nag_avg = std::make_shared<MeshRenderable>(flatShader, nag_avg_path + ".obj");
-	MeshRenderablePtr tete = std::make_shared<MeshRenderable>(flatShader, tete_path + ".obj");
-	HierarchicalRenderable::addChild(shell, nag_ard, true);
-	HierarchicalRenderable::addChild(shell, nag_arg, true);
-	HierarchicalRenderable::addChild(shell, nag_avd, true);
-	HierarchicalRenderable::addChild(shell, nag_avg, true);
-	HierarchicalRenderable::addChild(shell, tete, true);
-	nag_ard->addKeyframesFromFile(nag_ard_path + ".animation", 0.0, false);
-	nag_arg->addKeyframesFromFile(nag_arg_path + ".animation", 0.0, false);
-	nag_avd->addKeyframesFromFile(nag_avd_path + ".animation", 0.0, false);
-	nag_avg->addKeyframesFromFile(nag_avg_path + ".animation", 0.0, false);
-	viewer.addRenderable(shell);
+void initialize_scene(Viewer &viewer)
+{
+	// Shaders
+	ShaderProgramPtr cartoonShader = std::make_shared<ShaderProgram>(
+		"../../sfmlGraphicsPipeline/shaders/phongVertex.glsl",
+		"../../sfmlGraphicsPipeline/shaders/cartoonFragment.glsl");
+	ShaderProgramPtr cubeMapShader = std::make_shared<ShaderProgram>("../../sfmlGraphicsPipeline/shaders/cubeMapVertex.glsl",
+																	 "../../sfmlGraphicsPipeline/shaders/cubeMapFragment.glsl");
+	viewer.addShaderProgram(cubeMapShader);
+	viewer.addShaderProgram(cartoonShader);
+	
+	// Materials
+	MaterialPtr pearl = Material::Pearl();
+	
+	// Cubemap
+	std::string cubemap_dir = "../Textures/skybox";
+	auto cubemap = std::make_shared<CubeMapRenderable>(cubeMapShader, cubemap_dir);
+	viewer.addRenderable(cubemap);
+	
+	// Objects
+	LightedMeshRenderablePtr ground = add_object(viewer, "Ground", pearl, cartoonShader);
+	LightedMeshRenderablePtr ocean = add_object(viewer, "Ocean", pearl, cartoonShader);
+	LightedMeshRenderablePtr palmiers = add_object(viewer, "Palmiers", pearl, cartoonShader);
+	LightedMeshRenderablePtr leaves = add_object(viewer, "Leaves", pearl, cartoonShader);
+
+	LightedMeshRenderablePtr shell = add_object(viewer, "Carapace", pearl, cartoonShader);
+	LightedMeshRenderablePtr nag_ard = add_object(viewer, "Nag-ArD", pearl, cartoonShader, shell);
+	LightedMeshRenderablePtr nag_arg = add_object(viewer, "Nag-ArG", pearl, cartoonShader, shell);
+	LightedMeshRenderablePtr nag_avd = add_object(viewer, "Nag-AvD", pearl, cartoonShader, shell);
+	LightedMeshRenderablePtr nag_avg = add_object(viewer, "Nag-AvG", pearl, cartoonShader, shell);
+	LightedMeshRenderablePtr tete = add_object(viewer, "Tete", pearl, cartoonShader, shell);
+
+	// Lights
+	// Sun
+	glm::vec3 d_direction = glm::normalize(glm::vec3(-1.0, -0.2, 1.0));
+	glm::vec3 d_ambient(0.4, 0.4, 0.6), d_diffuse(1.5, 1.5, 1.5), d_specular(1.5, 1.5, 1.5);
+	DirectionalLightPtr directionalLight = std::make_shared<DirectionalLight>(d_direction, d_ambient, d_diffuse, d_specular);
+	viewer.addDirectionalLight(directionalLight);
+
+	// Define a point light
+	// float p_constant = 1.0, p_linear = 5e-1, p_quadratic = 0.0;
+	// glm::vec3 p_position = glm::vec3(2, 5.0, 5.0);
+	// glm::vec3 p_ambient = glm::vec3(0.0, 0.0, 0.2);
+	// glm::vec3 p_diffuse = glm::vec3(3.0, 3.0, 3.0);
+	// glm::vec3 p_specular = glm::vec3(3.0, 3.0, 3.0);
+	// PointLightPtr pointLight1 = std::make_shared<PointLight>(p_position, p_ambient, p_diffuse, p_specular, p_constant, p_linear, p_quadratic);
+	// viewer.addPointLight(pointLight1);
+
+	// Animate camera
+	viewer.getCamera().addKeyframesFromFile("../Animation/Camera.animation", 0.0, false);
+	
+	// const std::string test_obj_path = "../ObjFiles/Tete.obj";
+	// LightedMeshRenderablePtr test = std::make_shared<LightedMeshRenderable>(cartoonShader, test_obj_path, pearl);
+	// test->addKeyframesFromFile("../Animation/Camera.animation", 0.0, false);
+	// viewer.addRenderable(test);
+
 	viewer.startAnimation();
 }
 
